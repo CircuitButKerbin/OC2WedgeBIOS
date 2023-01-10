d47e783e20b5cab8ee4bf942d485df212ac36f14
ChrisChrome <christophercookman@gmail.com>
2023-01-09 22:16:14 -0700

    Remove unneeded stuff from minify.sh

--- .github/scripts/minify.sh
+++ .github/scripts/minify.sh
@@ -1,20 +1,11 @@
-echo Creating Directory
 mkdir -p minified
-echo Creating Structure
 find src -type d -exec mkdir -p minified/{} \;
-echo Finding Files
 files=$(find src -name "*.lua" ! -name "*.min.js")
 pwd=$(pwd)
-echo Minifying files
 for file in $files
 do
-	echo $file
 	cat $file | luamin -c > minified/$file
 	# Change minified file name to end in .min.lua instead of .lua
 	mv minified/$file minified/${file%.lua}.min.lua
-	cat minified/$file #Debug
 done
-
-echo $?
-echo Done
 exit 0
