62c4f1e30576b7450bee904534e8f9e01edffa80
ChrisChrome <christophercookman@gmail.com>
2023-01-09 21:39:04 -0700

    I dont even know anymore

--- .github/scripts/minify.sh
+++ .github/scripts/minify.sh
@@ -6,7 +6,7 @@ echo Finding Files
 tmp1=$(find src -name "*.lua" ! -name "*.min.js")
 echo Minifying files
 echo $tmp1
-luamin -f $tmp1 >minified/$tmp1
+luamin -f ./$tmp1 > minified/$tmp1
 echo $?
 echo Done
 exit 0
