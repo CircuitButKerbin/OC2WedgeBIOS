885839c3b34b1e59ae0ca862d27e0ab074cdc3a3
ChrisChrome <christophercookman@gmail.com>
2023-01-09 21:36:11 -0700

    boof

--- .github/scripts/minify.sh
+++ .github/scripts/minify.sh
@@ -5,6 +5,7 @@ find src -type d -exec mkdir -p minified/{} \;
 echo Finding Files
 tmp1=$(find src -name "*.lua" ! -name "*.min.js")
 echo Minifying files
+echo $tmp1
 luamin -f $tmp1 >minified/$tmp1
 echo $?
 echo Done
