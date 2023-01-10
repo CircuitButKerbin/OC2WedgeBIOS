17df1055b8aa98d2c2b9073fdce52a837af3772b
ChrisChrome <christophercookman@gmail.com>
2023-01-09 21:31:36 -0700

    add echos?

--- .github/scripts/minify.sh
+++ .github/scripts/minify.sh
@@ -1,4 +1,8 @@
+echo Creating Directory
 mkdir -p minified
+echo Creating Structure
 find src -type d -exec mkdir -p minified/{} \;
+echo Finding Files
 tmp1=$(find src -name "*.lua" ! -name "*.min.js")
+echo Minifying files
 luamin -f $tmp1 >minified/$tmp1
